3403d630858103db1441be7cd8cf8d695987dae1
mygithubhas <50008220+mygithubhas@users.noreply.github.com>
2019-05-17 00:51:06 +0800

    Add files via upload

---
+++ Pytorch--caffe2/ossdfnnx.py
@@ -0,0 +1,88 @@
+# 一些包的导入
+import io
+import numpy as np
+
+from torch import nn
+import torch.utils.model_zoo as model_zoo
+import torch.onnx
+import torch.nn as nn
+import torch.nn.init as init
+
+class SuperResolutionNet(nn.Module):
+    def __init__(self, upscale_factor, inplace=False):
+        super(SuperResolutionNet, self).__init__()
+
+        self.relu = nn.ReLU(inplace=inplace)
+        self.conv1 = nn.Conv2d(1, 64, (5, 5), (1, 1), (2, 2))
+        self.conv2 = nn.Conv2d(64, 64, (3, 3), (1, 1), (1, 1))
+        self.conv3 = nn.Conv2d(64, 32, (3, 3), (1, 1), (1, 1))
+        self.conv4 = nn.Conv2d(32, upscale_factor ** 2, (3, 3), (1, 1), (1, 1))
+        #self.pixel_shuffle = nn.PixelShuffle(upscale_factor)#缩放3倍
+
+        self._initialize_weights()
+
+    def forward(self, x):
+        x = self.relu(self.conv1(x))
+        x = self.relu(self.conv2(x))
+        x = self.relu(self.conv3(x))
+        #x = self.pixel_shuffle(self.conv4(x))
+        return x
+
+    def _initialize_weights(self):
+        init.orthogonal_(self.conv1.weight, init.calculate_gain('relu'))
+        init.orthogonal_(self.conv2.weight, init.calculate_gain('relu'))
+        init.orthogonal_(self.conv3.weight, init.calculate_gain('relu'))
+        init.orthogonal_(self.conv4.weight)
+
+# 使用上面模型定义，创建super-resolution模型 
+torch_model = SuperResolutionNet(upscale_factor=3)
+model_url = 'https://s3.amazonaws.com/pytorch/test_data/export/superres_epoch100-44c6958e.pth'
+batch_size = 1    # just a random number
+
+# 使用预训练的权重初始化模型
+map_location = lambda storage, loc: storage
+if torch.cuda.is_available():
+    map_location = None
+torch_model.load_state_dict(model_zoo.load_url(model_url, map_location=map_location))
+
+# 将训练模式设置为falsesince we will only run the forward pass.
+torch_model.train(False)
+print(")))))))))))))))))))))))))))))))))))))))))))))))")
+x = torch.randn(batch_size, 1, 224, 224, requires_grad=True)
+
+# 导出模型
+torch_out = torch.onnx._export(torch_model,             # model being run
+                               x,                       # model input (or a tuple for multiple inputs)
+                               "super_resolution.onnx", # where to save the model (can be a file or file-like object)
+                               export_params=True)      # store the trained parameter weights inside the model file
+                               
+print(torch_out)
+import onnx
+import caffe2.python.onnx.backend as onnx_caffe2_backend
+
+# Load the ONNX ModelProto object. model is a standard Python protobuf object
+model = onnx.load("super_resolution.onnx")
+
+# prepare the caffe2 backend for executing the model this converts the ONNX model into a
+# Caffe2 NetDef that can execute it. Other ONNX backends, like one for CNTK will be
+# availiable soon.
+prepared_backend = onnx_caffe2_backend.prepare(model)
+
+# run the model in Caffe2
+
+# Construct a map from input names to Tensor data.
+# The graph of the model itself contains inputs for all weight parameters, after the input image.
+# Since the weights are already embedded, we just need to pass the input image.
+# Set the first input.
+print("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
+W = {model.graph.input[0].name: x.data.numpy()}
+
+# Run the Caffe2 net:
+c2_out = prepared_backend.run(W)[0]
+print(c2_out)
+
+
+# Verify the numerical correctness upto 3 decimal places
+np.testing.assert_almost_equal(torch_out.data.cpu().numpy(), c2_out, decimal=3)
+
+print("Exported model has been executed on Caffe2 backend, and the result looks good!")
